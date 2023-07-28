712797fc295dd23ae6429784c02561b3f0ce5638
Nate Shoffner <nate.shoffner@gmail.com>
2023-07-28 16:08:54 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src="./tenji.png" className="logo" alt="Tenji logo" />
+      <h1>Coming Soon&trade;</h1>
+      <p className="read-the-docs">
+        Tenji (展示) is an open-source project aimed at providing a reliable REST API for MyFigureCollection.net.
+      </p>
+    </>
+  )
+}
+
+export default App
