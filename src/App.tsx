0ba06677fa64a5f34ca7d3e0ede2b40c4b485c73
Nate Shoffner <nate.shoffner@gmail.com>
2023-07-29 04:13:44 -0400

    GitHub link

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 function App() {
   return (
     <>
       <img src="./tenji.png" className="logo" alt="Tenji logo" />
       <h1>Coming Soon&trade;</h1>
-      <p className="read-the-docs">
+      <p className="subtitle">
         Tenji (展示) is an open-source project aimed at providing a reliable REST API for MyFigureCollection.net.
+      </p>
+
+      <p>
+        <a href="https://github.com/Tenji-hin"><FontAwesomeIcon icon={faGithub} size="4x" /></a>
       </p>
     </>
   )
